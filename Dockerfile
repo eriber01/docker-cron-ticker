c9d00bea06a5ede4dd5e465ec869d8d42110c70a
eriber <tejeda.e@dhn.com.do>
2023-10-27 16:41:19 -0400

    multi platform

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,25 @@
 FROM node:20.9.0-alpine3.18
 
+# FROM --platform=$BUILDPLATFORM node:20.9.0-alpine3.18
+
 # crea el directorio app
 WORKDIR /app
 
-# copia los archivos
-COPY . .
+COPY package.json ./
 
 # instala las dependencias
 RUN npm install
+
+# copia los archivos
+COPY . .
 
 # realiza el test
 RUN npm run test
 
 # elimina archivos y dependecias no necesarios es pro
-RUN rm -rf test && rm -rf node_modules
+RUN rm -rf tests && rm -rf node_modules
+
+RUN npm install --pro
 
 # corre la imagen
 CMD [ "node", "app.js" ]
--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+// const express = require('express')
+
+// const app = express()
+// const port = 5000
 const { schedule } = require('node-cron')
 const { Task } = require('./tasks/tasks')
 
-console.log('Inicio');
 schedule('*/5 * * * * *', Task)
+
+// app.get('/', async (req, res) => {
+//   let time = 0
+
+//   schedule('*/5 * * * * *', () => {
+//     time = Task()
+//     console.log(time);
+//     res.status(200).json({
+//       message: time
+//     })
+//   })
+
+
+// })
+
+// app.listen(port, () => {
+//   console.log('app en el puerto: ', port);
+// })
+
+console.log('Inicio');
